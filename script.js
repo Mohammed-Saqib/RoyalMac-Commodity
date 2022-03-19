c64153e3418607e2768d162cec6e5d0b6401d3f2
Mohammed Saqib <mohammedsaqibbca62@gmail.com>
2022-03-20 02:03:55 +0530

    added animation for carousel

--- script.js
+++ script.js
@@ -4,6 +4,43 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:15,
+        responsiveClass:true,
+        loop:true,
+        autoplay:true,
+        dots: true,
+        slideSpeed: 200,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            150:{
+                items:1,
+                nav:true
+            },
+            450:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            750:{
+                items:2,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+});
+
 const timeine = gsap.timeline({defaults: {duration : 4 }})
 timeine
 .to('.crousel-img img',{opacity : 0.6, ease : 'power3.out' })
